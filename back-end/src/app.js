6e4091c4d218df96c5b7e0a63520a5a9dddadc14
Maneesh <maneeshkumaru721@gmail.com>
2026-05-12 13:55:52 +0530

    Solved vercel NOT_FOUND error

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -26,7 +26,7 @@ app.disable("x-powered-by");
 app.use(helmet());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:4173"],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
 );
