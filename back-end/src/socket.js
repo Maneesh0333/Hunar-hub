190e22ae6d21b2822b17d66faaa648840e5528a0
Maneesh <148631731+Maneesh0333@users.noreply.github.com>
2026-05-12 15:50:37 +0530

    Update socket.js

--- back-end/src/socket.js
+++ back-end/src/socket.js
@@ -1,8 +1,8 @@
 import { Server } from "socket.io";
 import jwt from "jsonwebtoken";
 
-import Message from "./models/message.model.js";
-import Conversation from "./models/conversation.model.js";
+import Message from "./models/Message.model.js";
+import Conversation from "./models/Conversation.model.js";
 import AppError from "./utils/AppError.js";
 
 export const initSocket = (server) => {
